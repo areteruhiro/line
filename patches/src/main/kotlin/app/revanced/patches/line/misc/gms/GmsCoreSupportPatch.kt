ae544b8faeb4050e3e7896302b2e443deda4ccc5
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-09 21:29:23 +0900

    Update GmsCoreSupportPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/gms/GmsCoreSupportPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/line/misc/gms/GmsCoreSupportPatch.kt
@@ -1,52 +1,26 @@
 package app.revanced.patches.line.misc.gms
-
 import app.revanced.patcher.patch.Option
-import app.revanced.patches.all.misc.resources.addResources
-import app.revanced.patches.all.misc.resources.addResourcesPatch
-import app.revanced.patches.line.misc.extension.sharedExtensionPatch
-import app.revanced.patches.line.misc.gms.Constants.LINE_PACKAGE_NAME
-import app.revanced.patches.line.misc.gms.Constants.REVANCED_LINE_PACKAGE_NAME
-
-
-import app.revanced.patches.shared.castContextFetchFingerprint
+import app.revanced.patches.line.misc.extension.extensionPatch
 import app.revanced.patches.shared.misc.gms.gmsCoreSupportPatch
-
-import app.revanced.patches.shared.primeMethodFingerprint
+import app.revanced.patches.line.gms.fingerprints.MainActivityFingerprint
 
 @Suppress("unused")
 val gmsCoreSupportPatch = gmsCoreSupportPatch(
-    fromPackageName = LINE_PACKAGE_NAME,
-    toPackageName = REVANCED_LINE_PACKAGE_NAME,
-    primeMethodFingerprint = primeMethodFingerprint,
-    earlyReturnFingerprints = setOf(
-        castContextFetchFingerprint,
-    ),
-    mainActivityOnCreateFingerprint = splashActivityOnCreateFingerprint,
-    extensionPatch = sharedExtensionPatch,
-    gmsCoreSupportResourcePatchFactory = ::gmsCoreSupportResourcePatch,
+    fromPackageName = LineConstants.PACKAGE_NAME,
+    toPackageName = LineConstants.SPOOFED_PACKAGE,
+    mainActivityOnCreateFingerprint = MainActivityFingerprint,
+    extensionPatch = extensionPatch,
+    gmsCoreSupportResourcePatchFactory = ::lineGmsCoreSupportResourcePatch,
 ) {
-
-    compatibleWith(
-        LINE_PACKAGE_NAME(
-            "15.12.2",
-        )
-    )
+    compatibleWith(LineConstants.PACKAGE_NAME)
+    dependsOn(extensionPatch)
 }
 
-private fun gmsCoreSupportResourcePatch(
+private fun lineGmsCoreSupportResourcePatch(
     gmsCoreVendorGroupIdOption: Option<String>,
 ) = app.revanced.patches.shared.misc.gms.gmsCoreSupportResourcePatch(
-    fromPackageName = LINE_PACKAGE_NAME,
-    toPackageName = REVANCED_LINE_PACKAGE_NAME,
+    fromPackageName = LineConstants.PACKAGE_NAME,
+    toPackageName = LineConstants.SPOOFED_PACKAGE,
+    spoofedPackageSignature = LineConstants.OFFICIAL_SIGNATURE,
     gmsCoreVendorGroupIdOption = gmsCoreVendorGroupIdOption,
-    spoofedPackageSignature = "61ed377e85d386a8dfee6b864bd85b0bfaa5af81",
-    executeBlock = {
-        addResources("shared", "misc.gms.gmsCoreSupportResourcePatch")
-
-        val gmsCoreVendorGroupId by gmsCoreVendorGroupIdOption
-    }
-) {
-    dependsOn(
-        addResourcesPatch,
-        )
-}
+)
