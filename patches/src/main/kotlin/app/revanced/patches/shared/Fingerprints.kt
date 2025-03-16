1ce15dad9f606e37a447f11367cc167b4769005d
areteruhiro <narumihiroki1114@gmail.com>
2025-03-17 00:48:19 +0900

    test

--- patches/src/main/kotlin/app/revanced/patches/shared/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/Fingerprints.kt
@@ -2,13 +2,12 @@ package app.revanced.patches.shared
 
 import app.revanced.patcher.fingerprint
 
-
 internal val castContextFetchFingerprint = fingerprint {
     strings("Error fetching PackageInfo.")
 }
 
 internal val primeMethodFingerprint = fingerprint {
-    strings("com.google.android.gms", "com.android.vending")
+    strings("com.google.android.GoogleCamera", "com.android.vending")
 }
 
 
