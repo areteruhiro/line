2d45b7fdad102ded17041197dba3a4d0f055fc53
areteruhiro <narumihiroki1114@gmail.com>
2025-12-07 16:54:01 +0900

    Revert "test"
    This reverts commit c234e63f

--- patches/src/main/kotlin/app/revanced/patches/shared/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/Fingerprints.kt
@@ -2,12 +2,13 @@ package app.revanced.patches.shared
 
 import app.revanced.patcher.fingerprint
 
+
 internal val castContextFetchFingerprint = fingerprint {
-    strings("Error fetching PackageInfo.")
+    strings("Error fetching CastContext.")
 }
 
 internal val primeMethodFingerprint = fingerprint {
-    strings("com.google.android.GoogleCamera", "com.android.vending")
+    strings("com.android.vending", "com.android.vending")
 }
 
 
