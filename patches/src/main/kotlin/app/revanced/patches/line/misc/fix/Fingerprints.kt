3637df37412cebc665ab5df0e6faac4cfef9072c
areteruhiro <narumihiroki1114@gmail.com>
2025-12-07 16:50:51 +0900

    Revert "署名偽装"
    This reverts commit 7c76b6ec41bfb95ef3333dd7a8d40989bd3b7d15.

--- patches/src/main/kotlin/app/revanced/patches/line/misc/fix/Fingerprints.kt
+++
@@ -1,6 +0,0 @@
-package app.revanced.patches.line.misc.fix
-
-
-import app.revanced.patcher.fingerprint
-
-internal val getAppSignatureFingerprint = fingerprint { strings("Signature check failed for ") }
