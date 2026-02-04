b98af341e5782e04d0a6eb0164a0cd402a1dd811
areteruhiro <narumihiroki1114@gmail.com>
2026-02-04 19:33:53 +0900

    change

--- patches/src/main/kotlin/app/revanced/patches/shared/misc/gms/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/misc/gms/Fingerprints.kt
@@ -1,7 +1,6 @@
 package app.revanced.patches.shared.misc.gms
 
 import app.revanced.patcher.fingerprint
-import app.revanced.patches.shared.misc.gms.EXTENSION_CLASS_DESCRIPTOR
 import com.android.tools.smali.dexlib2.AccessFlags
 
 const val GET_GMS_CORE_VENDOR_GROUP_ID_METHOD_NAME = "getGmsCoreVendorGroupId"
@@ -18,26 +17,22 @@ internal val googlePlayUtilityFingerprint = fingerprint {
 }
 
 internal val serviceCheckFingerprint = fingerprint {
-    accessFlags(AccessFlags.PUBLIC, AccessFlags.STATIC)
-    returns("V")
-    parameters("L", "I")
     strings("Google Play Services not available")
 }
 
-internal val gmsCoreSupportFingerprint = fingerprint {
-    accessFlags(AccessFlags.PRIVATE, AccessFlags.STATIC)
-    returns("Ljava/lang/String;")
-    parameters()
-    custom { method, classDef ->
-        method.name == "getGmsCoreVendorGroupId" && classDef.type == EXTENSION_CLASS_DESCRIPTOR
-    }
+internal val gmsCoreVendorStringFingerprint = fingerprint {
+    strings(
+        "com.google.android.gms",
+        "com.android.vending"
+    )
 }
+
 
 internal val originalPackageNameExtensionFingerprint = fingerprint {
     accessFlags(AccessFlags.PRIVATE, AccessFlags.STATIC)
     returns("Ljava/lang/String;")
     parameters()
     custom { methodDef, classDef ->
-        methodDef.name == "getOriginalPackageName" && classDef.type == EXTENSION_CLASS_DESCRIPTOR
+        methodDef.name == "getOriginalPackageName"
     }
 }
